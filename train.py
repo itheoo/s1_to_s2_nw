5449e195c511ffaec3377206a3f1258dff5436ba
itheoo <theopinzariu@gmail.com>
2021-06-02 00:59:12 +0300

    commit_02_06_2021

--- train.py
+++ train.py
@@ -56,8 +56,12 @@ testing_data_loader = DataLoader(dataset=test_set, num_workers=opt.threads, batc
 device = torch.device("cuda:0" if opt.cuda else "cpu")
 
 print('===> Building models')
-net_g = define_G(opt.input_nc, opt.output_nc, opt.ngf, 'batch', False, 'normal', 0.02, gpu_id=device)
-net_d = define_D(opt.input_nc + opt.output_nc, opt.ndf, 'basic', gpu_id=device)
+#net_g = define_G(opt.input_nc, opt.output_nc, opt.ngf, 'batch', False, 'normal', 0.02, gpu_id=device)
+#net_d = define_D(opt.input_nc + opt.output_nc, opt.ndf, 'basic', gpu_id=device)
+
+net_g = torch.load('train_model/netG_model_epoch_100.pth')
+net_d = torch.load('train_model/netD_model_epoch_100.pth')
+
 
 criterionGAN = GANLoss().to(device)
 criterionL1 = nn.L1Loss().to(device)
